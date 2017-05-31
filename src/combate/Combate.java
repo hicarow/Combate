390ed08f965daa89513309b835014bd820b8f33e
Hicaro <Hicaro@192.168.1.9>
2017-05-31 17:33:42 -0300

    new commit

---
+++ src/combate/Combate.java
@@ -0,0 +1,12 @@
+package combate;
+
+public class Combate {
+
+    public static void main(String[] args) {
+        
+        System.out.println(" Combate ");       
+        MenuPrincipal NovoJogo = new MenuPrincipal();
+        NovoJogo.posicionamento();
+    }
+    
+}

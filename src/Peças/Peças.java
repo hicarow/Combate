28cfba23085e7a9ae353273046dc8888f3723551
Charden <Charden@192.168.0.3>
2017-06-01 01:10:20 -0300

    ADICIONADO ATRIBUTO 'CPU' NA CLASSE PEÇAS PARA INDICAR SE A PEÇA É MOVIDA PELO JOGADOR(CPU FALSE) OU PELO CPU (TRUE)

--- src/Peças/Peças.java
+++ src/Peças/Peças.java
@@ -15,31 +15,43 @@ package Peças;
   
 abstract public class Peças{
     
+    protected boolean CPU;
     protected String nome;
     protected boolean visível;
     protected boolean móvel;
     
+    public boolean getCPU(){
+        return this.CPU;
+    }
+    public void setCPU(boolean novovalor){
+        this.CPU = novovalor;
+    }
+    
     public String getNome(){
         return this.nome;       
-    }
-            
+    }       
     public void setNome(String novonome){
         this.nome = novonome;
     }
     
     public boolean getVisível(){
-        return visível;
+        return this.visível;
     }
     public void setVisível(boolean novovalor){
-        visível = novovalor;
+        this.visível = novovalor;
     }
         
 
-    private batalha(){
+    public batalha(Peças 1, Peças 2){
         
-        if(this.)
+        if(this.nome == "Bomba"){
             
-         this.setVisível(true);
+        }
+        if(this.nome == "Bandeira"){
+        
+        }   
+         
+        this.setVisível(true);
     }
     
 }
--- src/combate/Combate.java
+++ src/combate/Combate.java
@@ -4,7 +4,7 @@ public class Combate {
 
     public static void main(String[] args) {
         
-        System.out.println(" Combate ");       
+        System.out.println(" Bem vindo ao jogo Combate! ");       
         MenuPrincipal NovoJogo = new MenuPrincipal();
         NovoJogo.posicionamento();
     }
